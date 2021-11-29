b5455f0b77d712a4260faa591b01cd1f9c538070
MiKueen <keshvi2298@gmail.com>
2021-11-29 18:12:23 +0530

    Updated actions

--- rasa/rasa-bot/actions/actions.py
+++ rasa/rasa-bot/actions/actions.py
@@ -11,7 +11,7 @@ from typing import Any, Text, Dict, List, Union
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import EventType
+from rasa_sdk.events import EventType, SlotSet
 from rasa.core.actions.forms import FormAction
 
 
@@ -53,48 +53,78 @@ class ActionInvalidMeeting(Action):
         return []
 
 
-class AskForSlotTime(Action):
+# class AskForSlotTime(Action):
+#     def name(self) -> Text:
+#         return "action_ask_reminder_form_time"
+
+#     def run(
+#         self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+#     ) -> List[EventType]:
+#         dispatcher.utter_message(text="utter_ask_reminder_form_time")
+#         return []
+
+
+# class AskForSlotReminderSubject(Action):
+#     def name(self) -> Text:
+#         return "action_ask_reminder_form_reminder_subject"
+
+#     def run(
+#         self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+#     ) -> List[EventType]:
+#         dispatcher.utter_message(text="utter_ask_reminder_form_reminder_subject")
+#         return []
+
+
+# class ReminderForm(FormAction):
+#     def name(self):
+#         return "reminder_form"
+
+#     @staticmethod
+#     def required_slots(tracker: "Tracker") -> List[Text]:
+#         return ["reminder_subject", "time"]
+
+#     def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
+#         return {
+#             "reminder_subject": [self.from_entity(entity="reminder_subject")],
+#             "time": [self.from_entity("time")],
+#         }
+
+#     def submit(
+#         self,
+#         dispatcher: CollectingDispatcher,
+#         tracker: Tracker,
+#         domain: Dict[Text, Any],
+#     ) -> List[Dict]:
+
+#         dispatcher.utter_message("utter_reminder_form_slots_values")
+#         return []
+
+
+class ValidateReminderForm(Action):
     def name(self) -> Text:
-        return "action_ask_reminder_form_time"
-
-    def run(
-        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
-    ) -> List[EventType]:
-        dispatcher.utter_message(text="utter_ask_reminder_form_time")
-        return []
-
-
-class AskForSlotSubject(Action):
-    def name(self) -> Text:
-        return "action_ask_reminder_form_reminder_subject"
-
-    def run(
-        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
-    ) -> List[EventType]:
-        dispatcher.utter_message(text="utter_ask_reminder_form_reminder_subject")
-        return []
-
-
-class ReminderForm(FormAction):
-    def name(self):
         return "reminder_form"
 
-    @staticmethod
-    def required_slots(tracker: "Tracker") -> List[Text]:
-        return ["reminder_subject", "time"]
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        required_slots = ["time", "reminder_subject"]
 
-    def slot_mappings(self) -> Dict[Text, Union[Dict, List[Dict]]]:
-        return {
-            "reminder_subject": [self.from_entity(entity="reminder_subject")],
-            "time": [self.from_entity("time")],
-        }
+        for slot_name in required_slots:
+            if tracker.slot.get(slot_name) is None:
+                # The slot is not filled yet, request the user to fill the slot next time.
+                return [SlotSet("requested_slot", slot_name)]
 
-    def submit(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> List[Dict]:
+        return [SlotSet("requested_slot", None)]
 
-        dispatcher.utter_message("utter_reminder_form_slots_values")
+
+class ActionCreateReminder(Action):
+    def name(self) -> Text:
+        return "action_create_reminder"
+
+    def run(
+        self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict
+    ) -> List[EventType]:
+        dispatcher.utter_message(template="utter_reminder_form_slots_values",
+                                time=tracker.get_slot("time"),
+                                reminder_subject=tracker.get_slot("reminder_subject"))
         return []
